7ada06dbb00c4e05e974e055d887d884cafa7403
Karthik351 <152361790+Karthik351@users.noreply.github.com>
2024-03-27 10:19:39 -0400

    Add files via upload

---
+++ CSA/script1.js
@@ -0,0 +1,4 @@
+document.getElementById('logout-button').addEventListener('click', function() {
+  // Redirect the user to the login page
+  window.location.href = 'login.html'; // Replace 'login.html' with the actual URL of your login page
+});
