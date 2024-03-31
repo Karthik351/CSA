4a107f0cfb6d9df96ea40d3840e24f12a8c8462b
Karthik351 <152361790+Karthik351@users.noreply.github.com>
2024-03-30 21:23:27 -0400

    Delete CSA directory

--- CSA/script1.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('logout-button').addEventListener('click', function() {
-  // Redirect the user to the login page
-  window.location.href = 'login.html'; // Replace 'login.html' with the actual URL of your login page
-});
